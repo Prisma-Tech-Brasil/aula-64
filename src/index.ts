5359fa36e62f43e273d5bfb562e9cfdc9a423e18
Wallace027Dev <wallaceofc@hotmail.com>
2025-02-05 15:31:41 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+const wallace = {
+  nome: "Wallace",
+  idade: 24,
+  redesSociais: [
+    { rede: "instagram", perfil: "wallace027dev" },
+    { rede: "linkedin", perfil: "wallace-dev" }
+  ]
+};
+
+const maria = {
+  nome: "Maria Vitória",
+  idade: 20,
+  redesSociais: [
+    { rede: "instagram", perfil: "apenas_franca" },
+  ]
+};
